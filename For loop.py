26a2f856c76c97d17b37d63258619b6fe74a6b64
Vijay khot <120132133+khvijay@users.noreply.github.com>
2024-02-21 19:45:39 +0530

    Add files via upload

---
+++ For loop.py
@@ -0,0 +1,28 @@
+# For loop example
+name = 'Vijay'
+for i in name:
+    print(i)
+    if (i =="j"):
+        print("This is something special")
+        
+
+# for loop example second   
+colors = ["Red", "green", "blue", "yellow", "orange"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i) 
+
+
+# For loop using Range method (first example)
+for V in range(1, 12, 3):
+    print(V)
+
+# For loop using Range method (second example)
+for k in range(23, 25, 45):
+    print(k)
+   
+        
+    
+
+    
